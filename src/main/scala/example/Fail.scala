17c5dc3367520b7f926ebe44d10dd2b412f7ef82
sanjiv sahayam <sanjsmailbox@gmail.com>
2014-05-16 14:20:57 +1000

    included FinagleHttpClient sample

--- src/main/scala/example/Fail.scala
+++ src/main/scala/example/Fail.scala
@@ -1,6 +1,5 @@
 package example
 
-import java.net.URI
 
 object Fail extends App {
 
@@ -9,25 +8,38 @@ object Fail extends App {
   import scala.concurrent._
   import scala.concurrent.duration._
   import java.net.URL
+  import java.net.URI
+  import com.twitter.util.Duration
 
+
+  val url = new URL("http://fonts.googleapis.com/css?family=Cabin+Sketch:700|Duru+Sans")
 
   private def encodeURL(url:URL): URL = {
     //create a URI from the URL to properly encode any special characters. Then extract a new URL
     new URI(url.getProtocol, url.getHost, url.getPath, url.getQuery, null).toURL
   }
 
-  def access(url:URL) {
-    implicit val httpClient = new ApacheHttpClient
+  def access(url:URL, clientName:String)(implicit client:HttpClient) {
 
+    println(s"Accessing $url from $clientName")
     val response = Await.result(HEAD(url).apply, 5.second)
     println(s"Response returned from ${url.toString} with code ${response.code}, body ${response.bodyString}")
   }
 
-  val url = new URL("http://fonts.googleapis.com/css?family=Cabin+Sketch:700|Duru+Sans")
+  def performAccessWith(client: => HttpClient, name:String) {
+    implicit val httpClient = client
+    access(encodeURL(url), s"$name [with fix]")
 
-  println("this works")
-  access(encodeURL(url))
+    try {
+      access(url, s"$name [without fix]")
+    } catch {
+      case x:Throwable => x.printStackTrace()
+    }
 
-  println("this doesn't")
-  access(new URL("http://fonts.googleapis.com/css?family=Cabin+Sketch:700|Duru+Sans"))
+    println()
+  }
+
+  performAccessWith(new ApacheHttpClient ,"ApacheHttpClient")
+  performAccessWith(new FinagleHttpClient(requestTimeout = Duration.fromSeconds(5)), "FinagleHttpClient")
+
 }
